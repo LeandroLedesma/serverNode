2c48f88d7616fb90bf68d9700bedafd5a84efa94
LeandroLedesma <leandro.d.ledesma@gmail.com>
2018-10-02 22:38:17 -0300

    fix bootstrap layout

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/js'));
+
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, '../pages/index.html'));
+  response.sendFile(path.join(__dirname, '/pages/index.html'));
 });
 app.get('/products', function(request, response) {
-    response.sendFile(path.join(__dirname, '../pages/products.html'));
+    response.sendFile(path.join(__dirname, '/pages/products.html'));
   });
 
+  
+  
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
