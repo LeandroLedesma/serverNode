14d5ccb7c13472c70f294111d37d959e232afc6d
LeandroLedesma <leandro.d.ledesma@gmail.com>
2018-10-03 00:10:10 -0300

    add styles css pages

--- server.js
+++ server.js
@@ -17,3 +17,13 @@ app.get('/products', function(request, response) {
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
+
+/*const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.get('/', (req, res) => res.sendFile (path.join(__dirname, 'index.html')))
+app.get('/productos', (req, res) => res.sendFile (path.join(__dirname, 'productos.html')))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))*/
